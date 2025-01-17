294034e41c6b1d24fb15db1d3ab21d60c4210cf7
Stepan <zollidan@yandex.ru>
2025-01-17 22:16:19 +0300

    empty commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,28 @@
-import type { Metadata } from "next"
-import { Nav } from "@/components/sidebar/nav"
-import { Header } from "@/components/header/header"
-import "./globals.css"
+import type { Metadata } from "next";
+import { Nav } from "@/components/sidebar/nav";
+import { Header } from "@/components/header/header";
+import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "aaf-bet.ru",
-    description: "",
-}
+  title: "aaf-bet.ru",
+  description: "",
+};
 
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className="flex min-h-screen w-full flex-col">
-                <main className="flex min-h-screen w-full flex-col">
-                    <Nav />
-                    <Header />
-                    {children}
-                </main>
-            </body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <body className="flex min-h-screen w-full flex-col">
+        <main className="flex min-h-screen w-full flex-col">
+          <Nav />
+
+          <Header />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
 }
