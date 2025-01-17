2bffae3693bcbdff8dc6a1c3dd245fd3a6fa2a9f
Андрей Романов <human1488pwnz@yandex.ru>
2025-01-17 18:20:01 +0300

    Remove useless

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
-"use client";
-
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <main>
-      <div className="flex justify-center items-center gap-4">
-        <Link
-          href="/parsers/marafon"
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300 ease-in-out"
-        >
-          Marafon
-        </Link>
-        <Link
-          href="/parsers/soccerway"
-          className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700 transition duration-300 ease-in-out"
-        >
-          Soccerway
-        </Link>
-      </div>
-    </main>
-  );
+const HomePage = () => {
+    return (
+        <article className="pl-14">
+            <div className="px-6">
+                HomePage
+            </div>
+        </article>
+    )
 }
+
+export default HomePage
