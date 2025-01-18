7ead8256555dec19b404a4eddc3e956c907567bb
Stepan <zollidan@yandex.ru>
2025-01-18 17:08:04 +0300

    fix: tasks

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,30 +1,30 @@
 "use client";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-interface Tasks {}
+// interface Tasks {}
 
 export default function ParsersPage() {
-  const [data, setData] = useState<Tasks[]>([]);
+  // const [data, setData] = useState<Tasks[]>([]);
 
-  function fetchFiles() {
-    const response = fetch("https://api.aaf-bet.ru/api/files/sql");
+  // function fetchFiles() {
+  //   const response = fetch("https://api.aaf-bet.ru/api/files/sql");
 
-    console.log(response);
+  //   console.log(response);
 
-    return response;
-  }
+  //   return response;
+  // }
 
-  useEffect(() => {
-    fetchFiles()
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetchFiles()
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     });
+  // }, []);
 
   return (
     <div>
-      <div>{}</div>
+      <div>Тут будут статусы выполнения задач.</div>
     </div>
   );
 }
