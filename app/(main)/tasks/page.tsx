8864e750e1756569c0d74b1748f84df6e4778ae6
Андрей Романов <human1488pwnz@yandex.ru>
2025-02-15 16:28:18 +0300

    refactor: change fetchTasks function, add loader

--- app/(main)/tasks/page.tsx
+++ app/(main)/tasks/page.tsx
@@ -2,12 +2,18 @@
 
 import { useCallback, useEffect, useState } from "react"
 
-import { TbFaceIdError } from "react-icons/tb"
+import { TbFaceIdError, TbLoader } from "react-icons/tb"
+import { TasksTable } from "@/components/tasks/tasks-table"
 
-export default function ParsersPage() {
-    const [tasks, setTasks] = useState()
+interface Task {
+    uuid: string
+    name: string
+    state: 'STARTED' | 'FAILURE' | 'SUCCESS'
+}
+
+const TasksPage = () => {
+    const [tasks, setTasks] = useState<Task[]>()
     const [isLoading, setIsLoading] = useState<boolean>(false)
-    // const [error, setError] = useState()
 
     const fetchTasks = useCallback(async () => {
         try {
@@ -15,7 +21,15 @@ export default function ParsersPage() {
 
             const response = await fetch('/api/tasks')
             const data = await response.json()
-            setTasks(data)
+            const arr = Object.values(data) as Task[]
+
+            const result = arr.map((item: Task) => ({
+                uuid: item.uuid,
+                name: item.name,
+                state: item.state,
+            }))
+
+            setTasks(result)
         } catch (error) {
             console.error(error)
         } finally {
@@ -27,18 +41,25 @@ export default function ParsersPage() {
         fetchTasks()
     }, [fetchTasks])
 
-    //TODO: сделать анимацию грустного лица
+    if (isLoading) return (
+        <article className="flex justify-center items-center pl-14">
+            <TbLoader className="size-10 animate-spin" />
+        </article>
+    )
+
     return (
-        isLoading
-            ? <div className="text-center">Загрузка...</div>
-            : <article className="flex justify-center">
-                {!tasks
-                    ? <div className="flex flex-col items-center gap-y-4">
-                        <TbFaceIdError className="size-20" />
-                        <h1>В данный момент задач нет</h1>
-                    </div>
-                    : <>table</>
-                }
-            </article>
+        <article className="flex justify-center pl-14">
+
+            {!tasks
+                ? <div className="flex flex-col items-center gap-y-4">
+                    <TbFaceIdError className="size-20" />
+                    <h1>В данный момент задач нет</h1>
+                </div>
+                : <TasksTable tasks={tasks} />
+            }
+
+        </article>
     )
 }
+
+export default TasksPage
