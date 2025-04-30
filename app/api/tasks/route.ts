c008345faca904d7b4e9bc43e9c1d1a72392db6f
Андрей Романов <human1488pwnz@yandex.ru>
2025-04-30 14:21:43 +0300

    refactor: update API endpoint and improve error logging in GET function

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -2,11 +2,13 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
 	try {
-		const response = await fetch('https://api.aaf-bet.ru/api/tasks/all')
-		const tasks = await response.json()
+		const response = await fetch('https://flower.aaf-bet.ru/api/tasks')
+		const data = await response.json()
+		const tasks = Object.values(data)
+
 		return NextResponse.json(tasks)
 	} catch (error) {
-		console.error('TASKS_ERROR', error)
+		console.error('TASKS ERROR', error)
 		return NextResponse.json(
 			{ message: 'Что-то пошло не так!' },
 			{ status: 500 }
