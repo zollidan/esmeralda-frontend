ad7c561a2cc2131e6cfeefd1b5cfc7c498f9da5e
Андрей Романов <human1488pwnz@yandex.ru>
2025-04-30 14:21:59 +0300

    refactor: simplify TasksPage component by removing state management and loading indicator

--- app/(main)/tasks/page.tsx
+++ app/(main)/tasks/page.tsx
@@ -1,63 +1,27 @@
-"use client"
+import { Task } from "@/types"
 
-import { useCallback, useEffect, useState } from "react"
-
-import { TbFaceIdError, TbLoader } from "react-icons/tb"
+import { TbFaceIdError } from "react-icons/tb"
 import { TasksTable } from "@/components/tasks/tasks-table"
 
-interface Task {
-    uuid: string
-    name: string
-    state: 'STARTED' | 'FAILURE' | 'SUCCESS'
-}
+const TasksPage = async () => {
+    const response = await fetch('https://flower.aaf-bet.ru/api/tasks')
+    const data = await response.json()
+    const tasks: Task[] = Object.values(data) as Task[]
 
-const TasksPage = () => {
-    const [tasks, setTasks] = useState<Task[]>()
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-
-    const fetchTasks = useCallback(async () => {
-        try {
-            setIsLoading(true)
-
-            const response = await fetch('/api/tasks')
-            const data = await response.json()
-            const arr = Object.values(data) as Task[]
-
-            const result = arr.map((item: Task) => ({
-                uuid: item.uuid,
-                name: item.name,
-                state: item.state,
-            }))
-
-            setTasks(result)
-        } catch (error) {
-            console.error(error)
-        } finally {
-            setIsLoading(false)
-        }
-    }, [])
-
-    useEffect(() => {
-        fetchTasks()
-    }, [fetchTasks])
-
-    if (isLoading) return (
-        <article className="flex justify-center items-center pl-14">
-            <TbLoader className="size-10 animate-spin" />
-        </article>
-    )
+    if (!!!tasks.length) {
+        return (
+            <article className="flex justify-center items-center pl-14">
+                <div className="flex flex-col items-center gap-y-4">
+                    <TbFaceIdError className="size-20" />
+                    <h1>Задач нет</h1>
+                </div>
+            </article>
+        )
+    }
 
     return (
         <article className="flex justify-center pl-14">
-
-            {!tasks
-                ? <div className="flex flex-col items-center gap-y-4">
-                    <TbFaceIdError className="size-20" />
-                    <h1>В данный момент задач нет</h1>
-                </div>
-                : <TasksTable tasks={tasks} />
-            }
-
+            <TasksTable tasks={tasks} />
         </article>
     )
 }
